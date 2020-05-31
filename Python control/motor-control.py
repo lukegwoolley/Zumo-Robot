582f932443e0a474abe81135bd392d35b3c3ff3a
Luke <sw1n15@soton.ac.uk>
2020-05-31 15:43:08 +0100

    base code for the web control
    waiting for python 3.6 to load so untested

--- Python control/motor-control.py
+++ Python control/motor-control.py
@@ -26,66 +26,66 @@ GPIO.setup(Motor_2_Reverse, GPIO.OUT)
  
 #--------------------------------------------
 def GO() :
-    print "Moving forwards"
+    print("Moving forwards")
     GPIO.output(Motor_1_Forward, GPIO.HIGH)
     GPIO.output(Motor_2_Forward, GPIO.HIGH)
  
 def BACKWARDS() :
-    print "Moving backwards"
+    print("Moving backwards")
     GPIO.output(Motor_1_Reverse, GPIO.HIGH)
     GPIO.output(Motor_2_Reverse, GPIO.HIGH)
  
 def RIGHT() :
-    print "Turning right"
+    print("Turning right")
     GPIO.output(Motor_1_Forward, GPIO.HIGH)
     GPIO.output(Motor_2_Reverse, GPIO.HIGH)
     time.sleep(0.5)
     STOP()
  
 def RIGHT_Cont() :
-    print "Turning right continuously"
+    print("Turning right continuously")
     GPIO.output(Motor_1_Forward, GPIO.HIGH)
     GPIO.output(Motor_2_Reverse, GPIO.HIGH)
  
 def LEFT() :
-    print "Turning left"
+    print("Turning left")
     GPIO.output(Motor_1_Reverse, GPIO.HIGH)
     GPIO.output(Motor_2_Forward, GPIO.HIGH)
     time.sleep(0.5)
     STOP()
  
 def LEFT_Cont() :
-    print "Turning left continuously"
+    print("Turning left continuously")
     GPIO.output(Motor_1_Reverse, GPIO.HIGH)
     GPIO.output(Motor_2_Forward, GPIO.HIGH)
  
  
-def STOP () :
-    print "stop"
+def STOP() :
+    print("stop")
     GPIO.output(Motor_1_Forward, GPIO.LOW)
     GPIO.output(Motor_2_Forward, GPIO.LOW)
     GPIO.output(Motor_1_Reverse, GPIO.LOW)
     GPIO.output(Motor_2_Reverse, GPIO.LOW)
  
- def MENU() :
-    print( "\n\r" )
-    print("  F  --> Forwards\n" )
-    print( "  B  --> Backwards\n" )
-    print( "  R  --> Right\n" )
-    print( "  RR --> Right continuously\n" )
-    print( "  L  --> Left\n" )
-    print( "  LL --> Left continuously\n" )
-    print( "  S  --> Stop\n" )
-    print( "  M  --> MENU\n" )
-    print( "\n" )
- 
-print "Running"
+def MENU() :
+    print("\n\r")
+    print("  F  --> Forwards\n")
+    print("  B  --> Backwards\n")
+    print("  R  --> Right\n")
+    print("  RR --> Right continuously\n")
+    print("  L  --> Left\n")
+    print("  LL --> Left continuously\n")
+    print("  S  --> Stop\n")
+    print("  M  --> MENU\n")
+    print("\n")
+
+print("Running")
  
 # --------------------------------------
 while 1:
-      print "What would you like to do? \n"
+      print("What would you like to do? \n")
       if input() == "finish" or input == "" or input() == "stop":
-         print "Program terminated."
+         print("Program terminated.")
          break
  
       elif input() == "M" :
