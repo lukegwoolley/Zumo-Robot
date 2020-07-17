5662cbd32e66a3a7886e75395473b2795a2243c0
lukegwoolley <35926332+lukegwoolley@users.noreply.github.com>
2020-07-17 17:41:19 +0100

    added threading and locks

--- Web control/Flask/flask-control.py
+++ Web control/Flask/flask-control.py
@@ -10,11 +10,8 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-import logging
 import threading
-import concurrent.futures
 import numpy as np
-from tasks import threaded_task 
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
@@ -22,6 +19,9 @@ app = Flask(__name__)
 i = 0
 dis = np.zeros(3)
 dis2 = 100 
+WALL = False
+GOF = False
+lock = RLock()
   
 #------------- PORTS --------------------
 Motor_1_Forward = 25
@@ -60,11 +60,14 @@ GPIO.setup(ECHO, GPIO.IN)
  
 #--------------------------------------------
 def GO() :
+    global GOF
     print("Moving forwards")
     GPIO.output(Motor_1_Forward, GPIO.HIGH)
     GPIO.output(Motor_2_Forward, GPIO.HIGH)
     GPIO.setup(Motor_1_Reverse, GPIO.LOW)
     GPIO.setup(Motor_2_Reverse, GPIO.LOW)
+    with lock:
+        GOF = True
  
 def BACKWARDS() :
     print("Moving backwards")
@@ -102,92 +105,100 @@ def LEFT_Cont() :
     GPIO.setup(Motor_1_Forward, GPIO.LOW)
     
 def STOP() :
+    global GOF
     print("stop")
     GPIO.output(Motor_1_Forward, GPIO.LOW)
     GPIO.output(Motor_2_Forward, GPIO.LOW)
     GPIO.output(Motor_1_Reverse, GPIO.LOW)
     GPIO.output(Motor_2_Reverse, GPIO.LOW)
+    with lock:
+        GOF = False
 
-def read_sensor() :
-
-    global dis2
-    # Set trigger to False (Low)
-    GPIO.output(TRIG, False)
-
-    # Allow module to settle
-    time.sleep(0.5)
-
-    # Send 10us pulse to trigger
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-    start = time.time()
-
-    while GPIO.input(ECHO)==0:
-        start = time.time()
-
-    while GPIO.input(ECHO)==1:
-        stop = time.time()
-
-    # Calculate pulse length
-    elapsed = stop-start
-
-    # Distance pulse travelled in that time is time
-    # multiplied by the speed of sound (cm/s)
-    distance = elapsed * 34300
-
-    # That was the distance there and back so halve the value
-    distance = distance / 2
-
-    i+=1
-    if i > 2:
-        i = 0
-        dis2 = np.min(dis) #the ultrasonic sensor only registers too high not really too low so this might be quicker
-        print(f"Dis: {dis2}")
-    dis[i] = distance
-    
 
 print("Running")
  
+#---------------------------------------
+def read_sensor() :
+    while 1:
+        global dis2
+        global i
+        global dis
+        global WALL
+        global GOF
+        # Set trigger to False (Low)
+        GPIO.output(TRIG, False)
+
+        # Allow module to settle
+        time.sleep(0.25)
+
+        # Send 10us pulse to trigger
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        start = time.time()
+
+        while GPIO.input(ECHO)==0:
+            start = time.time()
+
+        while GPIO.input(ECHO)==1:
+            stop = time.time()
+
+        # Calculate pulse length
+        elapsed = stop-start
+
+        # Distance pulse travelled in that time is time
+        # multiplied by the speed of sound (cm/s)
+        distance = elapsed * 34300
+
+        # That was the distance there and back so halve the value
+        distance = distance / 2
+
+        i+=1
+        if i > 2:
+            i = 0
+            dis2 = np.min(dis) #the ultrasonic sensor only registers too high not really too low so this might be quicker
+            if dis2 < 10:
+                WALL = True
+                if GOF:
+                    STOP()
+            else: 
+                WALL = False
+            print(f"Dis: {dis2}")
+        dis[i] = distance
 # --------------------------------------
 @app.route("/")
 def main():
-
-    print("Start thread")
-    thread = Thread(target=read_sensor,)
-    thread.daemon = True
-    thread.start()
-    print("Thread started")
     # Pass the template data into the template main.html and return it to the user
     return render_template('main_press.html', func=func)
 
 # The function below is executed when someone requests a URL with the mode and action in it:
 @app.route("/<changeMode>/<action>")
 def action(changeMode, action):
+    global dis2
+    global WALL
     # Convert the mode from the URL into a string:
     changeMode = str(changeMode)
     # If the action part of the URL is "on," execute the code indented below:
-    if 10 < dis2 < 20:
-       STOP()
-    elif dis2 < 10:
-       GO()
-    else:
-        if action == "on":
-            if changeMode == "GO":
+
+    if action == "on":
+        if changeMode == "GO":
+            if WALL:
+                STOP()
+            else:
                 GO()
-            elif changeMode == "BACKWARDS":
-                BACKWARDS()
-            elif changeMode == "LEFT":
-                LEFT_Cont()
-            elif changeMode == "RIGHT":
-                RIGHT_Cont()
-            elif changeMode == "STOP":
-                STOP()	  
-            # Save the status message to be passed into the template:
-            message = "Turned " + changeMode + " on."
-            print(message)
-        if action == "off":
-            STOP()
+        elif changeMode == "BACKWARDS":
+            BACKWARDS()
+        elif changeMode == "LEFT":
+            LEFT_Cont()
+        elif changeMode == "RIGHT":
+            RIGHT_Cont()
+        elif changeMode == "STOP":
+            STOP()	  
+        # Save the status message to be passed into the template:
+        message = "Turned " + changeMode + " on."
+        print(message)
+    if action == "off":
+        STOP()
 
    # Along with the pin dictionary, put the message into the template data dictionary:
     func[changeMode] = action
@@ -195,4 +206,7 @@ def action(changeMode, action):
     return render_template('main_press.html', func=func)
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', debug=True)
+    thread = threading.Thread(target=read_sensor)
+    thread.start()
+    app.run(host='0.0.0.0', debug=True)
+   
--- Web control/Flask/test.py
+++ Web control/Flask/test.py
@@ -1,8 +1,13 @@
 import sys
 import numpy as np
+from celery import Celery 
+
+app = Celery('test', broker='pyamqp://guest@localhost//')
 
 dis2 = 100
+print(dis2)
 
+@app.task
 def testing():
     dis = [10, 30, 100]
     global dis2 
